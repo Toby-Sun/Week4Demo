3ba2a0553dc8a52dedc0bd38f91ec24216bffce1
Toby-Sun <99042810+Toby-Sun@users.noreply.github.com>
2022-09-22 14:27:56 -0600

    Add validation Added validation to the form to ensure that the user typed in both first name and last name

--- src/java/servlets/HelloWorldSerlvet.java
+++ src/java/servlets/HelloWorldSerlvet.java
@@ -29,6 +29,16 @@ public class HelloWorldSerlvet extends HttpServlet {
         request.setAttribute("firstname", firstname);
         request.setAttribute("lastname", lastname);
         
+        if(firstname == null || firstname.equals("")|| lastname == null || lastname.equals("")){
+            request.setAttribute("firstname", firstname);
+            request.setAttribute("lastname", lastname);
+                
+            
+            getServletContext().getRequestDispatcher("/WEB-INF/helloWorldForm.jsp")
+                .forward(request, response);
+                    return;
+        }
+        
         getServletContext().getRequestDispatcher("/WEB-INF/sayHello.jsp")
                 .forward(request, response);
         
